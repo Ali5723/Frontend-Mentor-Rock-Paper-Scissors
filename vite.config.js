3ef49277243e4c5089d0675cf1370835f628e97d
Ali <ali.moh.2000.ly@gmail.com>
2026-05-03 02:24:05 +0300

    Fix Build 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Frontend-Mentor-Rock-Paper-Scissors/",
+  base: "./",
+  // base: "/Frontend-Mentor-Rock-Paper-Scissors/",
   // server: {
   //   host: true,
   //   port: 5173,
